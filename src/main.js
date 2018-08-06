adb6284c0644c9360839fc1dd62a93d313386dc5
dongu <fordongu@gmaill.com>
2018-08-06 09:35:49 +0800

    x5

--- src/main.js
+++ src/main.js
@@ -190,6 +190,19 @@ let keyboard = {
             })
         },
         close(){
+            if (this.disabled) return
+            navigator.vibrate ? navigator.vibrate(50) : (navigator.webkitVibrate ? navigator.webkitVibrate(50) : '')
+            if (this.type == 'pay' && this.str.length > 0) {
+                if (this.str.indexOf(".") != -1) {
+                    if (!this.str.split(".")[1]) {
+                        this.str += "00";
+                    } else if (this.str.split(".")[1].length === 1) {
+                        this.str += "0";
+                    }
+                } else {
+                    this.str += ".00";
+                }
+            }
             this.$emit("close");
         }
     },
